5643437840ab2ab9aaf951324fc610d0b5b2ac62
Fereydoun Memarzanjany <VioletVillain@outlook.com>
2020-03-11 23:07:12 +0330

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
+// Crate-level documentation (Overview) resides in the 'README.md' file.
+
+// #![no_std] // Async will soon be available on non-std programs. https://github.com/rust-lang/rust/pull/69033
+
+#![feature(concat_idents)]
+
 pub mod core;
 
-/*
-pub mod input;
-*/
-
+/// A module which is typically glob imported from:
+///
+/// ```
+/// use rasm::prelude::*;
+/// ```
 pub mod prelude {
     pub use crate::{
         core,
